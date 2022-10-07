c710c3b05090f9c33538b0d91c03a260f3a1d4ce
Abel Gasque <abelgasque20@gmail.com>
2022-10-07 17:01:26 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UPD8.CSharp.CustomerEntity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
